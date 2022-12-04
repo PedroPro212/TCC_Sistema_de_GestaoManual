02910b6bf492839ad98a265fe82e22e66e0c5c0e
Pedro Oliveira <96023606+PedroPro212@users.noreply.github.com>
2022-12-04 15:48:22 -0300

    Sistema de login pt1 finalizado

--- GestaoManual/GestaoManual/Login/Logar.aspx.cs
+++ GestaoManual/GestaoManual/Login/Logar.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,16 +11,45 @@ namespace GestaoManual.Login
     public partial class Logar : System.Web.UI.Page
     {
         protected static string senha;
-
+        private MySqlConnection connection = new MySqlConnection(SiteMaster.ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
+            txtSenha.MaxLength = 6;
             if (!IsPostBack)
                 txtSenha.TextMode = TextBoxMode.Password;
         }
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-            
+            connection.Open();
+            MySqlCommand comando = new MySqlCommand();
+            comando.CommandText = "SELECT * FROM login WHERE registro = @registro AND senha = @senha AND id_acesso";
+            comando.Parameters.AddWithValue("@registro", txtRegistro.Text);
+            comando.Parameters.AddWithValue("@senha", txtSenha.Text);
+            MySqlDataReader dr;
+                 
+            try
+            {
+                comando.Connection = connection;
+                dr = comando.ExecuteReader();
+                if(dr.HasRows)
+                {
+                    Response.Redirect("/Supervisor/Responsavel.aspx");
+                }
+                else
+                {
+                    SiteMaster.AlertPersonalizado(this, "Registro ou senha incorretos");
+                    txtRegistro.Text = "";
+                }              
+            }
+            catch(Exception ex)
+            {
+                SiteMaster.AlertPersonalizado(this, "Aconteceu o seguinte erro: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }         
         }
 
         protected void chkMostrarSenha_CheckedChanged(object sender, EventArgs e)
--- GestaoManual/GestaoManual/Site.Master.cs
+++ GestaoManual/GestaoManual/Site.Master.cs
@@ -9,11 +9,28 @@ namespace GestaoManual
 {
     public partial class SiteMaster : MasterPage
     {
-        public static string ConnectionString = "Server=127.0.0.1;User ID=root;Password=;Database=metalurgicasaveiro";
-
+        //public static string ConnectionString = "Server=127.0.0.1;User ID=root;Password=;Database=metalurgicasaveiro";   Filipe e Pedro Note
+        public static string ConnectionString = "Server=127.0.0.1;User ID=root;Password=1234;Database=metalurgicasaveiro";  // Computador Pedro
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        public static void AlertPersonalizado(Page page, string mensagem)
+        {
+            page.ClientScript.RegisterStartupScript(
+                page.GetType(),
+                "MessageBox" + Guid.NewGuid(),
+                "<script language='javascript'>swal('" + mensagem + "');</script>"
+                );
+        }
+
+        public static void AlertPersonalizado(Page page, string mensagem, string pagina)
+        {
+            page.ClientScript.RegisterStartupScript(
+                page.GetType(),
+                "MessageBox" + Guid.NewGuid(),
+                "<script language='javascript'>swal('" + mensagem + "');window.location = '" + pagina + "'</script>"
+                );
         }
     }
 }
