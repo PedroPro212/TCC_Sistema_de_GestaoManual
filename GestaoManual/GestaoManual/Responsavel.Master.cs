01af6cebd348182fa410dd903c5eba42ec7a3224
Pedro Oliveira <96023606+PedroPro212@users.noreply.github.com>
2022-12-11 22:32:23 -0300

    Sistema de nome

--- GestaoManual/GestaoManual/Responsavel.Master.cs
+++ GestaoManual/GestaoManual/Responsavel.Master.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,21 @@ namespace GestaoManual
 {
     public partial class Responsavel : System.Web.UI.MasterPage
     {
+        private MySqlConnection connection = new MySqlConnection(SiteMaster.ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
+            connection.Open();
+            int id = Convert.ToInt32(Session["Login"].ToString());
 
+            var rdr = new MySqlCommand("select id, substring_index(nome, ' ', -1) as UltimoNome, substring_index(nome, ' ', 1) as PrimeiroNome from funcionarios WHERE id=" + id, connection).ExecuteReader();
+            while (rdr.Read())
+            {
+                var segundo = new ListItem(rdr.GetString("UltimoNome"), rdr.GetInt32("id").ToString());
+                var primeiro = new ListItem(rdr.GetString("PrimeiroNome"), rdr.GetInt32("id").ToString());
+                var nome = segundo + ", " + primeiro;
+                lblNome.Text = nome;
+            }
+            connection.Close();
         }
     }
 }
--- GestaoManual/GestaoManual/Responsavel.Master.designer.cs
+++ GestaoManual/GestaoManual/Responsavel.Master.designer.cs
@@ -33,6 +33,15 @@ namespace GestaoManual
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Controle lblNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNome;
+
+        /// <summary>
         /// Controle content.
         /// </summary>
         /// <remarks>
