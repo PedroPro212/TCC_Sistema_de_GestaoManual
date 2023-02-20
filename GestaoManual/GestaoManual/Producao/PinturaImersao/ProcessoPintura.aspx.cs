54eca9414eb9b79018622a32b96222c7c5ce5cd0
Pedro Oliveira <96023606+PedroPro212@users.noreply.github.com>
2023-02-20 15:28:05 -0300

    Finalização ForntEnd tela de Chamado Suporte

--- GestaoManual/GestaoManual/Producao/PinturaImersao/ProcessoPintura.aspx.cs
+++ GestaoManual/GestaoManual/Producao/PinturaImersao/ProcessoPintura.aspx.cs
@@ -16,42 +16,41 @@ namespace GestaoManual.Producao.PinturaImersao
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            connection.Open();
-            int id = Convert.ToInt32(Session["Login"].ToString());
-
-            var reader = new MySqlCommand("select id, substring_index(nome, ' ', -1) as UltimoNome, substring_index(nome, ' ', 1) as PrimeiroNome from funcionarios WHERE id=" + id, connection).ExecuteReader();
-            while(reader.Read())
-            {
-                var segundo = new ListItem(reader.GetString("UltimoNome"), reader.GetInt32("id").ToString());
-                var primeiro = new ListItem(reader.GetString("PrimeiroNome"), reader.GetInt32("id").ToString());
-                var nome = segundo + ", " + primeiro;
-                lblNome.Text = nome;
-            }
-            connection.Close();
-
-            connection.Open();
-            int idP = Convert.ToInt32(Session["Produto"].ToString());
-            var rdr = new MySqlCommand($"SELECT id, descricao FROM produto WHERE id={idP}", connection).ExecuteReader();
-            while(rdr.Read())
-            {
-                var produto = new ListItem(rdr.GetString("descricao"), rdr.GetInt32("id").ToString());
-                lblProduto.Text = produto.ToString();
-            }
-            connection.Close();
-
-            connection.Open();
-            int idS = Convert.ToInt32(Session["Setor"].ToString());
-            var ler = new MySqlCommand($"SELECT id, descricao FROM setor WHERE id={idS}", connection).ExecuteReader();
-            while (ler.Read())
-            {
-                var setor = new ListItem(ler.GetString("descricao"), ler.GetInt32("id").ToString());
-                lblSetor.Text = setor.ToString();
-            }
-            connection.Close();
-
-            //dataHoraIni = Request.Form["lblDataHoraInicio"];
             try
             {
+                connection.Open();
+                int id = Convert.ToInt32(Session["Login"].ToString());
+
+                var reader = new MySqlCommand("select id, substring_index(nome, ' ', -1) as UltimoNome, substring_index(nome, ' ', 1) as PrimeiroNome from funcionarios WHERE id=" + id, connection).ExecuteReader();
+                while(reader.Read())
+                {
+                    var segundo = new ListItem(reader.GetString("UltimoNome"), reader.GetInt32("id").ToString());
+                    var primeiro = new ListItem(reader.GetString("PrimeiroNome"), reader.GetInt32("id").ToString());
+                    var nome = segundo + ", " + primeiro;
+                    lblNome.Text = nome;
+                }
+                connection.Close();
+
+                connection.Open();
+                int idP = Convert.ToInt32(Session["Produto"].ToString());
+                var rdr = new MySqlCommand($"SELECT id, descricao FROM produto WHERE id={idP}", connection).ExecuteReader();
+                while(rdr.Read())
+                {
+                    var produto = new ListItem(rdr.GetString("descricao"), rdr.GetInt32("id").ToString());
+                    lblProduto.Text = produto.ToString();
+                }
+                connection.Close();
+
+                connection.Open();
+                int idS = Convert.ToInt32(Session["Setor"].ToString());
+                var ler = new MySqlCommand($"SELECT id, descricao FROM setor WHERE id={idS}", connection).ExecuteReader();
+                while (ler.Read())
+                {
+                    var setor = new ListItem(ler.GetString("descricao"), ler.GetInt32("id").ToString());
+                    lblSetor.Text = setor.ToString();
+                }
+                connection.Close();
+
                 connection.Open();
                 var reader2 = new MySqlCommand($"SELECT op.id, op.registro, op.idMaquina, ma.id, ma.nome FROM operador AS op, maquina AS ma WHERE op.id = ma.id AND op.registro={id}", connection).ExecuteReader();
                 while (reader2.Read())
@@ -63,7 +62,7 @@ namespace GestaoManual.Producao.PinturaImersao
             }
             catch(Exception ex)
             {
-
+                SiteMaster.AlertPersonalizado(this, "Aconteceu um erro inesperado:\n" + ex);
             }
 
         }
@@ -71,8 +70,6 @@ namespace GestaoManual.Producao.PinturaImersao
         protected void btnFinalizar_Click(object sender, EventArgs e)
         {
             int id = Convert.ToInt32(Session["Login"].ToString());
-
-
 
             DateTime dataFim = DateTime.Now;
             lblTeste.Text = teste.Value;
--- GestaoManual/GestaoManual/Supervisor/Maquina/Produtividade.aspx
+++ GestaoManual/GestaoManual/Supervisor/Maquina/Produtividade.aspx
@@ -2,7 +2,7 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="content" runat="server">
-
+    <asp:Calendar runat="server"></asp:Calendar>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="footer" runat="server">
 </asp:Content>
