e4f2b590215ababee80113202fb8607d323cbdf9
Filipower888 <filipefcoura888@gmail.com>
2023-02-27 22:14:20 -0300

    atribuir operador à máquina (botão pendente)

--- GestaoManual/GestaoManual/Supervisor/Atribuir/Atribuir.aspx
+++ GestaoManual/GestaoManual/Supervisor/Atribuir/Atribuir.aspx
@@ -2,9 +2,9 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="content" runat="server">
-    <asp:DropDownList runat="server" ID="ddlOperador"></asp:DropDownList>
-    <asp:DropDownList runat="server" ID="ddlMaquina"></asp:DropDownList>
-    <asp:Button runat="server" ID="btnAtribuir" Text="ATRIBUIR" />
+    <asp:DropDownList runat="server" ID="ddlOperador" AutoPostBack="true" OnSelectedIndexChanged="ddlOperador_SelectedIndexChanged"></asp:DropDownList>
+    <asp:DropDownList runat="server" ID="ddlMaquina" Enabled="false"></asp:DropDownList>
+    <asp:Button runat="server" ID="btnAtribuir" Text="ATRIBUIR" OnClick="btnAtribuir_Click"/>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="footer" runat="server">
 </asp:Content>
--- GestaoManual/GestaoManual/Supervisor/Atribuir/Atribuir.aspx.cs
+++ GestaoManual/GestaoManual/Supervisor/Atribuir/Atribuir.aspx.cs
@@ -11,59 +11,90 @@ namespace GestaoManual.Supervisor.Atribuir
     public partial class Atribuir : System.Web.UI.Page
     {
         private MySqlConnection connection = new MySqlConnection(SiteMaster.ConnectionString);
+        int acesso;
+        int idSetor;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            string registro = Session["login"].ToString();
-            int acesso = 0;
-            int idSetor = 0;
+            if (!IsPostBack)
+            {
+                string registro = Session["login"].ToString();
 
+                connection.Open();
+                MySqlCommand comando = new MySqlCommand("SELECT l.id_acesso, f.id_setor FROM login AS l, funcionarios AS f WHERE l.registro = f.id AND l.registro = @registro", connection);
+                comando.Parameters.AddWithValue("@registro", registro);
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    acesso = reader.GetInt32("id_acesso");
+                    idSetor = reader.GetInt32("id_setor");
+                }
+                connection.Close();
+
+                if (acesso == 4)
+                {
+                    connection.Open();
+                    ddlOperador.Items.Clear();
+                    var operador = new ListItem("", "0");
+                    ddlOperador.Items.Add(operador);
+                    reader = new MySqlCommand("SELECT id, nome FROM funcionarios", connection).ExecuteReader();
+                    while (reader.Read())
+                    {
+                        operador = new ListItem(reader.GetString("nome"), reader.GetInt32("id").ToString());
+                        ddlOperador.Items.Add(operador);
+                    }
+                    connection.Close();
+                }
+
+                if (acesso == 2)
+                {
+                    connection.Open();
+                    ddlOperador.Items.Clear();
+                    reader = new MySqlCommand($"SELECT id, nome FROM funcionarios WHERE id_setor={idSetor}", connection).ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var operador = new ListItem(reader.GetString("nome"), reader.GetInt32("id").ToString());
+                        ddlOperador.Items.Add(operador);
+                    }
+                    connection.Close();
+                }
+            }
+        }
+
+        protected void ddlOperador_SelectedIndexChanged(object sender, EventArgs e)
+        {
             connection.Open();
-            MySqlCommand comando = new MySqlCommand("SELECT l.id_acesso, f.id_setor FROM login AS l, funcionarios AS f WHERE l.registro = f.id AND l.registro = @registro", connection);
-            comando.Parameters.AddWithValue("@registro", registro);
+            MySqlCommand comando = new MySqlCommand($"SELECT id_setor FROM funcionarios WHERE id={ddlOperador.SelectedValue}", connection);
             MySqlDataReader reader = comando.ExecuteReader();
-
             if (reader.Read())
             {
-                acesso = reader.GetInt32("id_acesso");
                 idSetor = reader.GetInt32("id_setor");
             }
             connection.Close();
 
-            if (acesso == 4)
+            connection.Open();
+            ddlMaquina.Items.Clear();
+            reader = new MySqlCommand($"SELECT id, nome FROM maquina WHERE id_setor={idSetor}", connection).ExecuteReader();
+            while (reader.Read())
             {
-                connection.Open();
-                ddlOperador.Items.Clear();
-                reader = new MySqlCommand("SELECT id, nome FROM funcionarios", connection).ExecuteReader();
-                while (reader.Read())
-                {
-                    var operador = new ListItem(reader.GetString("nome"), reader.GetInt32("id").ToString());
-                    ddlOperador.Items.Add(operador);
-                }
-                connection.Close();
+                var maquina = new ListItem(reader.GetString("nome"), reader.GetInt32("id").ToString());
+                ddlMaquina.Items.Add(maquina);
             }
+            connection.Close();
 
-            if (acesso == 2)
+            if (ddlOperador.SelectedValue == "0")
             {
-                connection.Open();
-                ddlOperador.Items.Clear();
-                reader = new MySqlCommand($"SELECT id, nome FROM funcionarios WHERE id_setor ={idSetor}", connection).ExecuteReader();
-                while (reader.Read())
-                {
-                    var operador = new ListItem(reader.GetString("nome"), reader.GetInt32("id").ToString());
-                    ddlOperador.Items.Add(operador);
-                }
-                connection.Close();
-
-                connection.Open();
                 ddlMaquina.Items.Clear();
-                reader = new MySqlCommand($"SELECT id, nome FROM maquina WHERE id_setor={idSetor}", connection).ExecuteReader();
-                while (reader.Read())
-                {
-                    var maquina = new ListItem(reader.GetString("nome"), reader.GetInt32("id").ToString());
-                    ddlMaquina.Items.Add(maquina);
-                }
+                ddlMaquina.Enabled = false;
             }
+            else
+                ddlMaquina.Enabled = true;
+        }
+
+        protected void btnAtribuir_Click(object sender, EventArgs e)
+        {
+            //dfkçhkjasdkjsdlkjkjasdklbdskjdfkbjçdçjkkjdfkbdfçlkasdfkjlkjasdklkjsdlçjdsjkiiii.ldskjkjjjdjjklçjakjjkkkkkkkakjçjkjdssoiencnhhOIIIIIIIIOoijcjjiiuliukdkkkkkkkçljfnncvjkidsichlalhghkldjdddddsppapappppppppppenahfjdkjpedrogaydjhasdifbhvueuuuhauuchhhhhhhjjjjjjjjjjççççççççivvvvvuhfkljsdsdsdlkjsdkjhewwwwwwwwwwwwwwwwwwwwpoioiuipoiiiiiiouijhcv  bkbcvvkjhkjhhhfudsiucahuadsufhlkjksdauhuiouekjjjjjjjjhvkoiiiiiidhccccccccccccçacjbvnzkakduflallllllllllllllllkdhfhdiuo
         }
     }
 }
