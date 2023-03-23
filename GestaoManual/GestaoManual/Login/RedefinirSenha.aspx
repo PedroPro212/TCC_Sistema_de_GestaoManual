4b560619fe434a27eb618bad0eebed8fd81e6aeb
Pedro Oliveira <pedrogamer212004@gmail.com>
2023-03-22 22:16:19 -0300

    Adição botão para redefinir/recuperar a senha

---
+++ GestaoManual/GestaoManual/Login/RedefinirSenha.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Logar.Master" AutoEventWireup="true" CodeBehind="RedefinirSenha.aspx.cs" Inherits="GestaoManual.Login.RedefinirSenha" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="content" runat="server">
+
+    <style>
+        body{
+            background-color:#F9B463;
+        }
+    </style>
+
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="footer" runat="server">
+</asp:Content>
--- GestaoManual/GestaoManual/Login/index.aspx.designer.cs
+++ GestaoManual/GestaoManual/Login/index.aspx.designer.cs
@@ -42,6 +42,15 @@ namespace GestaoManual.Login
         protected global::System.Web.UI.WebControls.CheckBox chkMostrarSenha;
 
         /// <summary>
+        /// Controle btnRedefinirSenha.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRedefinirSenha;
+
+        /// <summary>
         /// Controle btnLogin.
         /// </summary>
         /// <remarks>
